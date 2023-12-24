775a112d7fea8650267cc633bf25c499dd1d92ad
viquise <48965121+viquise@users.noreply.github.com>
2023-12-24 20:22:56 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,26 @@
+import inquirer from "inquirer";
+import qr from 'qr-image';
+import fs from 'fs'
+
+// var inquirer = require('inquirer');
+
+inquirer
+  .prompt([
+    {
+      message:  "Enter the url you want to seach for: ",
+      name: 'URL'
+    },
+    ])
+  .then((answers) => {
+    const url = answers.URL;
+    var qr_svg = qr.image(url);
+    qr_svg.pipe(fs.createWriteStream('qr_img.png'));
+    // qr.imageSync(url);
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
