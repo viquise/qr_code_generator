80ba015bdaaab9a5f1f7869616d406ffd84929af
viquise <48965121+viquise@users.noreply.github.com>
2023-12-25 05:58:38 +0100

    nothing changed

--- index.js
+++ index.js
@@ -15,7 +15,10 @@ inquirer
     const url = answers.URL;
     var qr_svg = qr.image(url);
     qr_svg.pipe(fs.createWriteStream('qr_img.png'));
-    // qr.imageSync(url);
+    fs.writeFile("url.txt", url, (err)=>{
+        if (err) throw err;
+        console.log("Your url has been saved");
+    })
   })
   .catch((error) => {
     if (error.isTtyError) {
